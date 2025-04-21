4d0e639a26c9ff1a53c5b719f5f41bd0b19c945a
alexxoblomov <116631643+alexxoblomov@users.noreply.github.com>
2025-04-21 12:40:33 +0500

    refactor

--- madout_apk/apk_path.py
+++ madout_apk/apk_path.py
@@ -1,2 +1,4 @@
+from pathlib import Path
+
 class MadOutV16:
-    app = 'C:\\Android-versions_16-9136f58dd5-1744698918.apk'
+    APP_PATH = Path(__file__).parent.parent / "madout_apk" / "Android-versions_16-9d707efe69-1745219397.apk"
